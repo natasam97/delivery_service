6b714fd4a4ea9fa1edd8e640ce1a22f26a9a7a86
Milos <miloszrilic@gmail.com>
2017-04-17 16:28:58 +0200

    Project setup - upgrade_v1.0.0.

--- delivery-service/src/app/app.module.ts
+++ delivery-service/src/app/app.module.ts
@@ -4,17 +4,42 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { MapsComponent } from './maps/maps.component';
+import { HistoryComponent } from './history/history.component';
+import { CourierComponent } from './courier/courier.component';
+import { SenderComponent } from './sender/sender.component';
+
+import { UserService } from './user/user.service';
+import { AuthService } from './user/auth/auth.service';
+import { GuardService } from './user/guard/guard.service';
+
+import { AgmCoreModule } from 'angular2-google-maps/core';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    LoginComponent,
+    MapsComponent,
+    HistoryComponent,
+    CourierComponent,
+    SenderComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyAkQ2qaepxFLi7zbCG4_1P13QmxzN3n-KM'
+    })
   ],
-  providers: [],
+  providers: [
+    UserService,
+    AuthService,
+    GuardService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
